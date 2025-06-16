600b9e01f668047fab1a678b65022be73786f162
Rinat_Work <rinat.islamov.04@gmail.com>
2025-06-16 16:29:06 +0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='kangaroo-tools',
-    version='0.1.1',
+    version='0.1.2',
     author='Rinat',
     author_email='rinat.islamov.04@gmail.com',
     description='Библиотека для очистки текста от мусора, HTML, эмодзи и стоп-слов',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
+    # long_description=open('README.md').read(),  ← Убери или закомментируй
+    # long_description_content_type='text/markdown',
     url='https://github.com/Seraphim04174/kangaroo-tools.git',
     packages=find_packages(),
     classifiers=[
