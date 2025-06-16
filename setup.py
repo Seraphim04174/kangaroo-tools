46a325352dabdaec0995daf297ee0699d01e626b
Rinat_Work <rinat.islamov.04@gmail.com>
2025-06-16 16:06:53 +0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='textcleaner',
-    version='0.1.0',
+    name='kangaroo-tools',
+    version='0.1.1',
     author='Rinat',
-    author_email='your@email.com',
+    author_email='rinat.islamov.04@gmail.com',
     description='Библиотека для очистки текста от мусора, HTML, эмодзи и стоп-слов',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/textcleaner',
+    url='https://github.com/Seraphim04174/kangaroo-tools.git',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
